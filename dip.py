6f125ae68c7ecb5ac13d011473a4034dcc19b7d5
stiralno <gglebcchesnakov@gmail.com>
2026-03-23 16:29:02 +0700

    v2

--- dip.py
+++ dip.py
@@ -1,107 +1,295 @@
 import simpy
 import random
-import time
-import os
+from typing import Union, NoReturn, List, Generator
 
 
 FOLLOWER = 'follower'
 CANDIDATE = 'candidate'
 LEADER = 'leader'
 ELECTION_TIMEOUT = 5
-VOTE_PROBABILTY = 0.6
+VOTING_TIMEOUT = 2
+NODE_PROBABILITY_FAILURE = 0.05
+HEARTBIT = 5
+LAMBDA = 0.1
+EXPO_TIME = 1
+UNIFORM_TIME = 0
+UNIFORM_A = 1
+UNIFORM_B = 2
+ELECTION_MIN = HEARTBIT + 0.1
+ELECTION_MAX = ELECTION_MIN + 1
+SLEEPING_TIME_MIN = 4
+SLEEPING_TIME_MAX = 7
+
+PROCESSED_ENTRIES = 0
+
+
+def get_sleeping_time() -> float:
+    return random.uniform(SLEEPING_TIME_MIN, SLEEPING_TIME_MAX)
+
+
+def get_election_time() -> float:
+    return random.uniform(ELECTION_MIN, ELECTION_MAX)
+
+
+def get_next_request_time() -> Union[NoReturn, float]:
+    if EXPO_TIME:
+        return random.expovariate(lambd=LAMBDA)
+    if UNIFORM_TIME:
+        return random.uniform(UNIFORM_A, UNIFORM_B)
+    raise ValueError("Time error")
+
 
 class RaftNode:
-    def __init__(self, env, name, cluster):
-        self.env = env
-        self.name = name
-        self.cluster = cluster
-        self.state = FOLLOWER
-        self.vote_count = 0
-        self.election_timeout = ELECTION_TIMEOUT
-        self.last_heard_from_leader = env.now
-        self.election_process = env.process(self.run())
-        self.voted = False
-    
-    def run(self):
-        while True:
-            if self.state == FOLLOWER:
-                yield self.env.timeout(self.election_timeout)
-                if self.env.now - self.last_heard_from_leader > self.election_timeout:
-                    write_to_file(f"{self.name} не получил ответа и переходит в состояние кандидата.", self.env)
-                    self.state = CANDIDATE
-                    self.start_election()
-            elif self.state == CANDIDATE:
-                yield self.env.timeout(random.uniform(1, 2))
-                if self.vote_count > len(self.cluster) // 2:
-                    self.state = LEADER
-                    write_to_file(f"{self.name} стал лидером", self.env)
-                    self.free_nodes_votes()
-                    self.start_replication()
-                else:
-                    self.state = FOLLOWER
-                    write_to_file(f"{self.name} стал фолловером после неудачных выборов.", self.env)
-            elif self.state == LEADER:
-                yield self.env.timeout(random.uniform(1, 2))
-                if (random.random() < 0.05): # лидер может молчать
-                    silence_time = random.uniform(self.election_timeout - 1, self.election_timeout + 2)
-                    write_to_file(f'Лидер молчит {silence_time + 1} ед. времени', self.env)
-                    self.state = FOLLOWER # лидер стал фоловером
-                    yield self.env.timeout(silence_time) # время молчания лидера
-                else:
-                    self.start_replication()
-                    #self.replicate_logs()
+    processed_entries: int = 0  # обработанные запросы
+    #leader: RaftNode
 
-    def start_election(self):
-        self.vote_count = 1
-        for node in self.cluster:
-            if node != self:
-                node.receive_vote_request(self)
+    def __init__(self, env: simpy.Environment, name: str, cluster: List):
+        env.process(self.run())
+        self.timeout_proc: simpy.Process = None
+        self.env: simpy.Environment = env
+        self.name: str = name
+        self.cluster: List = cluster
 
-    def free_nodes_votes(self):
-        for node in self.cluster:
-            node.voted = False
-
-    def receive_vote_request(self, candidate):
-        if self.state == FOLLOWER:
-            if random.random() < VOTE_PROBABILTY and not self.voted:
-                write_to_file(f"{self.name} голосует за {candidate.name}.", self.env)
-                candidate.vote_count += 1
-                self.last_heard_from_leader = self.env.now
-                self.voted = True
-
-    def start_replication(self):
-        write_to_file(f'{self.name} начал отправлять конфигурацию', self.env)
+        self.state: str = FOLLOWER
+        self.vote_count: int = 0
+        self.election_timeout: float = get_election_time()
+        self.last_heard_from_leader: float = env.now
+        #self.election_process: simpy.Process = env.process(self.run())
+        self.voted: bool = False
+        self.index: int = 0
+        self.term: int = 0
         
-        for node in self.cluster:
-            if node != self:
-                node.replicate_conf()
+        self.timeout_event: simpy.Event
+        self.requested_vote_event: simpy.Event = self.env.event()
+        self.requested_vote_event.callbacks.append(self.vote_callback)
+        #self.requested_vote_event.callbacks.append(self.set_new_timeout_event)  # новый таймаут когда проголосовал
+        self.ack_log_event: simpy.Event = self.env.event()  # запрос индексации
+        self.ack_log_event.callbacks.append(self.ack_log_callback)
+        #self.ack_log_event.callbacks.append(self.set_new_timeout_event)  # новый таймаут когда принял запись
+        self.commit_log_event: simpy.Event = self.env.event()  # запрос коммита
+        self.commit_log_event.callbacks.append(self.commit_log_callback)
+        #self.commit_log_event.callbacks.append(self.set_new_timeout_event)  # новый таймаут когда закомитил запись
+        self.nodes_voted_timeout_event: simpy.Timeout
+        self.heartbit_event: simpy.Timeout
+        self.entry_event: simpy.Timeout
+        self.is_shutdown: bool = False  # узел не раб
+        self.ack_count: int = 0  # число узлов, подтвердивших запись
+        self.commited_count: int = 0  # число узлов, закоммитивших запись
+        self.leader: RaftNode
 
-    def replicate_conf(self):
+        self.entries = 0
 
-        write_to_file(f"{self.name} принимает конфигурацию.", self.env)
-        self.receive_heartbeat() 
-    
-    def receive_heartbeat(self):
+    def run(self) -> Generator:
+        while True:
+            if random.random() <= NODE_PROBABILITY_FAILURE:
+                sl = get_sleeping_time()
+                write_to_file(f'{self.name} заснул на {sl}', self.env)
+                self.is_shutdown = True
+                sleeping_timeout = self.env.timeout(sl)
+                # if self.state == LEADER:
+                #     yield self.env.all_of([self.entry_event, sleeping_timeout]) # если приходит запрос лидер почему то просыпается
+                # else:
+                yield self.env.all_of([sleeping_timeout])
+                self.is_shutdown = False
+            if self.state == FOLLOWER:
+                write_to_file(f'{self.name} стал фоловером', self.env)
+                self.set_new_timeout_event()
+                # фоловер ждет какое то событие(добавить ли синхронизацию?)
+                write_to_file(f'{self.name} {self.election_timeout}', self.env)
+                result = yield self.env.any_of([self.timeout_proc, self.ack_log_event, self.commit_log_event, self.requested_vote_event]) #возвращает словарь с тригер событиями?
+            elif self.state == CANDIDATE:  # передать кандидата в ивент
+                # узел становится кандидатом, отсылает запросы, те обновляют таймаут, таймаут должен обновлятся каждый раз
+                # если не получил большинство, замолкает, выбирается другой кандидат, если не получил большинство замолкает
+                # если получил половину, начинаем новый терм?
+                self.set_new_voted_timeout_event()
+                self.term += 1
+                write_to_file(f'{self.name} стал кандидатом и запрашивает голоса',  self.env)
+                # сделать события окончания голосования за кандидата. ждать any of с хартбитом от лидера, вдруг проснется.
+                self.request_votes() # запрашиваем голоса
+                result = yield self.env.any_of([self.nodes_voted_timeout_event, self.ack_log_event, self.requested_vote_event])  #  ждём таймаут выборов, или когда проснется лидер, или когда(возможно) к нам постучаться проголосовать 
+            elif self.state == LEADER:
+                write_to_file(f'{self.name} лидер', self.env)
+                result = yield self.env.any_of([self.heartbit_event, self.entry_event, self.ack_log_event])  # ждем либо когда будем посылать хартбит, либо когда придёт запрос, либо чужой хартбит от вохможного лидера
+
+    def heartbit_callback(self, event) -> None:
+        write_to_file(f'{self.name} посылает heartbit',  self.env)
+        if self.entries > 0:
+            self.index += 1
+        for i in self.cluster:
+            if i != self:
+                i.ack_log_event.succeed(value=self)
+        self.env.process(self._process_ack())
+        #yield self.env.timeout(0.0001)
+        # write_to_file(f'Лидер {self.name}, запросов:{self.entries} подтверждений: {self.ack_count}', self.env)
+        # if self.entries > 0 and self.ack_count > len(self.cluster) // 2:  # отделить комит
+        #     write_to_file(f'{self.name} посылает комиты',  self.env)
+        #     for i in self.cluster:
+        #         if i != self:
+        #             i.commit_log_event.succeed(value=self)
+        #     self.entries -= 1
+        #     RaftNode.processed_entries += 1  # считаем запрос обработанным
+        # self.ack_count = 0
+        # self.set_new_heartbit_event()
+
+    def _process_ack(self):
+        yield self.env.timeout(0)
+        write_to_file(f'Лидер {self.name}, запросов:{self.entries} подтверждений: {self.ack_count}', self.env)
+        if self.entries > 0 and self.ack_count > len(self.cluster) // 2:  # отделить комит
+            write_to_file(f'{self.name} посылает комиты',  self.env)
+            for i in self.cluster:
+                if i != self:
+                    i.commit_log_event.succeed(value=self)
+            self.entries -= 1
+            RaftNode.processed_entries += 1  # считаем запрос обработанным
+        self.ack_count = 0
+        self.set_new_heartbit_event()
+
+    def entry_callback(self, event) -> None:
+        leader = event.value
+        leader.entries += 1
+        write_to_file(f'Лидер {leader.name} принял запрос, необработанных записей: {self.entries}',  leader.env)
+        #leader.index += 1
+        leader.set_new_entry_event(leader)
+
+    def nodes_voted_timeout_callback(self, event) -> None:
+        write_to_file(f'{self.name} проверка результатов выборов',  self.env)
+        if self.vote_count > len(self.cluster) // 2:
+            write_to_file(f'{self.name} стал лидером',  self.env)
+            self.state = LEADER
+            self.set_new_heartbit_event()
+            self.set_new_entry_event(self)
+        else:
+            self.state = FOLLOWER
+            write_to_file(f'{self.name} проиграл выборы и стал фоловером',  self.env)
+        self.vote_count = 0
+
+    def vote_callback(self, event) -> None:
+        candidate = event.value
+        #self.set_new_timeout_event()  # обновили таймаут
+        self.set_new_vote_event()  # обновили возможность голосовать 
+        if not self.voted:
+            write_to_file(f'{self.name} голосует',  self.env)
+            if candidate.term == self.term and candidate.index >= self.index or candidate.term > self.term:
+                candidate.vote_count += 1  # проголосовали за кандидата
+                self.voted = True
+                write_to_file(f'{self.name} проголосовал за {candidate.name}',  self.env)
+                if candidate.term > self.term:
+                    self.term += 1
+                    write_to_file(f'{self.name} увеличил терм из-за кандидата',  self.env)
+
+    def timeout_callback(self, event=None) -> None:
+        if not self.is_shutdown:
+            write_to_file(f'{self.name} таймаут колбек. Последний хартбит от лидера: {self.last_heard_from_leader}', self.env)
+            if self.env.now - self.last_heard_from_leader > self.election_timeout:
+                write_to_file(f"{self.name} не получил ответа и переходит в состояние кандидата.", self.env)
+                self.state = CANDIDATE
+
+    def ack_log_callback(self, event) -> None:
+        write_to_file(f'К {self.name} пришел запрос ack', self.env)
+        self.leader = event.value
+        if self.state == CANDIDATE and self.term < self.leader.term:
+            self.state = FOLLOWER
+        if not self.is_shutdown and not self.leader.is_shutdown and self.term <= self.leader.term: # что с индексом? если терм принимающего выше?
+            if self.state == LEADER:  # если получили терм поновее(или такой же?) зануляем запросы на лидера и передаем их новому
+                self.leader.entries = self.entries
+                self.entries = 0
+            self.state = FOLLOWER
+            if self.leader.entries > 0 and self.index + 1 != self.leader.index:  # фоловер не синхронизирован
+                write_to_file(f'{self.name} не синхронизирован с {self.leader.name}, терм лидера {self.leader.term}, индекс лидера {self.leader.index}, терм узла {self.term} индекс узла {self.index}',  self.env)
+                self.index = self.leader.index - 1  # синхронизировали(доработка)
+
+            if self.leader.entries > 0:  # если это не простой хартбит
+                self.leader.ack_count += 1
+                self.voted = False  # после выборов нужно установить в False
+                self.vote_count = 0
+                self.index += 1
+                write_to_file(f'{self.name} подтвердил запрос {self.leader.name}, терм лидера {self.leader.term}, индекс лидера {self.leader.index}, терм узла {self.term} индекс узла {self.index}',  self.env)
+            self.voted = False  # после выборов нужно установить в False
+            self.vote_count = 0
         self.last_heard_from_leader = self.env.now
+        self.term = self.leader.term
+        self.set_new_ack_event()
+
+    def commit_log_callback(self, event) -> None:
+        if not self.is_shutdown and not self.leader.is_shutdown and self.term <= self.leader.term: # and self.index + 1 == self.leader.index ?
+            self.leader.commited_count += 1
+            self.index = self.leader.index  # не += 1, т.к. если обновится терм, то индекс должен слететь на 0
+            write_to_file(f'{self.name} закоммитил запрос {self.leader.name}, терм лидера {self.leader.term}, индекс лидера {self.leader.index}, терм {self.term} индекс {self.index}', self.env)
+        self.set_new_commit_event()
+
+    def request_votes(self) -> None:
+        write_to_file(f'{self.name} начал собирать голоса', self.env)
+        self.vote_count = 1  # голосуем за себя
+        for node in self.cluster:  # посылаем запросы
+            if node != self:
+                node.set_new_vote_event()
+                node.requested_vote_event.succeed(value=self)
+
+    def set_new_ack_event(self):
+        self.ack_log_event = self.env.event()
+        self.ack_log_event.callbacks.append(self.ack_log_callback)
+
+    def set_new_commit_event(self):
+        self.commit_log_event = self.env.event()
+        self.commit_log_event.callbacks.append(self.commit_log_callback)
+
+    def set_new_vote_event(self):
+        self.requested_vote_event = self.env.event()
+        self.requested_vote_event.callbacks.append(self.vote_callback)
+
+    def timeout_process(self):  # процесс для таймаута, т.к. Timeout все равно сработает в any_of после отмены
+        try:
+            yield self.env.timeout(self.election_timeout)
+            self.timeout_callback()
+        except simpy.Interrupt:
+            write_to_file(f'{self.name} таймаут прерван в {self.env.now}', self.env)
+    
+    def set_new_timeout_event(self):
+        self.election_timeout = get_election_time()
+        write_to_file(f'{self.name} следующий таймаут: {self.env.now + self.election_timeout}', self.env)
+        if self.timeout_proc is not None and self.timeout_proc.is_alive:
+            self.timeout_proc.interrupt()
+        self.timeout_proc = self.env.process(self.timeout_process())
+
+        # self.timeout_event = self.env.timeout(self.election_timeout)
+        # self.timeout_event.callbacks.append(self.timeout_callback)
+
+    def set_new_voted_timeout_event(self):
+        self.nodes_voted_timeout_event = self.env.timeout(VOTING_TIMEOUT)
+        self.nodes_voted_timeout_event.callbacks.append(self.nodes_voted_timeout_callback)
+
+    def set_new_heartbit_event(self):
+        self.heartbit_event = self.env.timeout(HEARTBIT)
+        self.heartbit_event.callbacks.append(self.heartbit_callback)
+
+    def set_new_entry_event(self, leader):
+        self.entry_event = self.env.timeout(get_next_request_time(), value=leader)
+        self.entry_event.callbacks.append(self.entry_callback)
+
 
 def write_to_file(text, env):
     with open('results.txt', mode='a') as f:
         f.write(f'\n{env.now}: {text}')
 
 
-
 def run_simulation():
     env = simpy.Environment()
-    cluster = [RaftNode(env, f"Node{i}", []) for i in range(5)]
+    cluster = [RaftNode(env, f"Узел {i}", []) for i in range(5)]
+    cluster[0].state = LEADER
+    cluster[0].set_new_entry_event(cluster[0])
+    cluster[0].set_new_heartbit_event()
 
     for node in cluster:
         node.cluster = cluster
+        node.leader = cluster[0]
     env.run(until=300)
     clear_file()
+
 
 def clear_file():
     input('Enter:')
     with open('results.txt', 'w') as f:
         f.write('')
+
 
 run_simulation()
